f6847b109eace1ca74e699371c44b87843d93a87
kwokkw <68409369+kwokkw@users.noreply.github.com>
2024-04-03 00:42:27 -1000

    Add files via upload
    initial commit

---
+++ let-const-exercise.js
@@ -0,0 +1,18 @@
+// ES Global Constants
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+// ES2015 Version
+const PI = 3.14;
+
+// What is the difference between var and let?
+// `var` variables can be redeclared and reassgined and have a function scope. `let` variables can be reassgined but cannot be redeclared, and they have a block scope.
+
+// What is the difference between var and const?
+// `var` variables can be redeclared and reassgined and have a function scope. `const` variables cannot be redeclared or reassgined, and they have a block scope.
+
+// What is the difference between let and const?
+// `let` variables can be reassgined but cannot be redeclared. `const` variables cannot be redeclared or reassgined
+
+// What is hoisting?
+// When using `var`, variables declartion portion are moved to the top of the scope.
